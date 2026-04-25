dc2bda8cd8ec8e86b3d11df77eeb0ccb041dfdd6
Tran Dai Duong <duongtd@bglobalcorp.com>
2026-04-25 15:30:25 +0700

    refactor and adding jupyter

--- src/config.py
+++
@@ -1,7 +0,0 @@
-DATA_PATH = "../data/"
-
-TARGET = "is_churn"
-ID_COL = "msno"
-
-TEST_SIZE = 0.2
-RANDOM_STATE = 42
--- src/evaluate.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import log_loss
-
-def evaluate(model, X_val, y_val):
-    pred = model.predict_proba(X_val)[:, 1]
-    loss = log_loss(y_val, pred)
-
-    print(f"Log Loss: {loss:.4f}")
-    return loss
